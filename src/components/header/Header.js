f310f09b01d0edb5cd4f028cd28aea550cf19a8d
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-12 21:06:00 +0200

    header to stateless functional component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Topbar from './Topbar';
 import Intro from './Intro';
 import Laptop from './Laptop';
 import curve from '../../image/curve-cropped.png';
 import './Header.scss';
 
-class Header extends Component {
-  state = {  }
-  render() {
-    return (
-      <header className="header" role="banner">
-        <div className="container">
-          <Topbar />
-          <Intro />
-          <Laptop />
-        </div>
-        <img className="header__curve" src={curve} alt="" aria-hidden="true" />
-      </header>
-    );
-  }
+const Header = () => {
+  return (
+    <header className="header" role="banner">
+      <div className="container">
+        <Topbar />
+        <Intro />
+        <Laptop />
+      </div>
+      <img className="header__curve" src={curve} alt="" aria-hidden="true" />
+    </header>
+  );
 }
 
 export default Header;
